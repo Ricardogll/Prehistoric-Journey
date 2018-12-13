3b6e4c437d778a21a41eb6004e8879eeafd6e89a
Sersius <segoro97@msn.com>
2018-12-13 18:12:57 +0100

    Chicken now printed (only one for test)

--- Prehistoric-Journey/Motor2D/Chicken.cpp
+++ Prehistoric-Journey/Motor2D/Chicken.cpp
@@ -12,24 +12,59 @@
 
 Chicken::Chicken(int x, int y, pugi::xml_node& config, EntityTypes type) :Entity(x, y, type)
 {
+	pugi::xml_node node_entity = config.child("chicken");
+
+	if (node_entity != NULL) {
+		LoadVariablesXML(node_entity);
+
+		pugi::xml_node animations = node_entity.child("animations");
+
+		SetAnimations(animations.child("idle").child("animation"), idle);
+		idle.speed = animations.child("idle").attribute("speed").as_float();
+		idle.loop = animations.child("idle").attribute("loop").as_bool();
+
+		texture = App->tex->Load(spritesheet.GetString());
+		collider = App->collision->AddCollider({ (int)position.x + collider_offset.x, (int)position.y + collider_offset.y, collider_dimensions.x, collider_dimensions.y }, COLLIDER_COLLECTABLE, (j1Module*)App->entities);
+		death_sound = App->audio->LoadFx(death_sound_folder.GetString());
+	}
+	state = IDLE;
 }
 
 Chicken::~Chicken()
 {
 }
 
-void Chicken::OnCollision(Collider * c1, Collider * c2)
-{
-}
-
 void Chicken::Update(float dt)
 {
+	dt_current = dt;
+	AnimationsApplyDt();
 }
 
 void Chicken::Draw()
 {
+	current_animation = &idle;
+
+	App->render->Blit(texture, position.x, position.y, &(current_animation->GetCurrentFrame()));
 }
 
 void Chicken::AnimationsApplyDt()
 {
+	if (anim_speed_flag == false) {
+		idle_anim_speed = idle.speed;
+
+		anim_speed_flag = true;
+	}
+	else
+	{
+		idle.speed = idle_anim_speed * dt_current;
+	}
+}
+
+void Chicken::OnCollision(Collider * c1, Collider * c2)
+{
+	if (c2->type == COLLIDER_PLAYER) {
+		App->audio->PlayFx(death_sound, 0);
+		App->collision->EraseCollider(collider);
+		App->tex->UnLoad(texture);
+	}
 }
